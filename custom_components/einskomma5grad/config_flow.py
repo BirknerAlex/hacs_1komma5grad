ba5cf3de5a57b1b09c4d4d15bf2214da6f67e8ec
Alexander Birkner <alex.birkner@gmail.com>
2025-12-01 18:09:08 +0100

    feat: Added custom scrape interval settings
    Solves #15

--- custom_components/einskomma5grad/config_flow.py
+++ custom_components/einskomma5grad/config_flow.py
@@ -8,14 +8,15 @@ from typing import Any
 import requests
 import voluptuous as vol
 
-from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant import config_entries
+from homeassistant.config_entries import ConfigFlowResult
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
 
 from .api.client import Client
 from .api.error import AuthenticationError
-from .const import DOMAIN
+from .const import DOMAIN, DEFAULT_SCAN_INTERVAL, MIN_SCAN_INTERVAL, MAX_SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,6 +24,9 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_USERNAME): str,
         vol.Required(CONF_PASSWORD): str,
+        vol.Optional(
+            CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL
+        ): vol.All(vol.Coerce(int), vol.Range(min=MIN_SCAN_INTERVAL, max=MAX_SCAN_INTERVAL)),
     }
 )
 
@@ -32,26 +36,19 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data[CONF_USERNAME], data[CONF_PASSWORD]
-    # )
     api_client = Client(data[CONF_USERNAME], data[CONF_PASSWORD])
 
     try:
-        return await hass.async_add_executor_job(api_client.get_user)
+        return await hass.async_add_executor_job(lambda: api_client.get_user())
     except AuthenticationError as e:
         raise InvalidAuth(e.message) from e
     except requests.RequestException as e:
         raise CannotConnect from e
     finally:
-        await hass.async_add_executor_job(api_client.close)
+        await hass.async_add_executor_job(lambda: api_client.close)
 
 
-class ConfigFlow(ConfigFlow, domain=DOMAIN):
+class HeartbeatConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for 1KOMMA5GRAD."""
 
     VERSION = 1
@@ -72,17 +69,33 @@ class ConfigFlow(ConfigFlow, domain=DOMAIN):
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
+                # Save username/password as data and store scan interval in options
+                options = {CONF_SCAN_INTERVAL: int(user_input.get(CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL))}
                 return self.async_create_entry(
                     title=user["firstName"] + " " + user["lastName"],
                     data={
                         CONF_USERNAME: user_input[CONF_USERNAME],
                         CONF_PASSWORD: user_input[CONF_PASSWORD],
                     },
+                    options=options,
                 )
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        """Return the options flow handler for this config entry."""
+        _LOGGER.debug("ConfigFlow.async_get_options_flow called for %s", getattr(config_entry, "entry_id", config_entry))
+        return OptionsFlowHandler(config_entry)
+
+
+# Also provide a module-level factory for older/newer HA compatibility
+def async_get_options_flow(config_entry):
+    """Module-level factory for options flow (compatibility layer)."""
+    _LOGGER.debug("module-level async_get_options_flow called for %s", getattr(config_entry, "entry_id", config_entry))
+    return OptionsFlowHandler(config_entry)
 
 
 class CannotConnect(HomeAssistantError):
@@ -91,3 +104,34 @@ class CannotConnect(HomeAssistantError):
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options for the integration."""
+
+    def __init__(self, config_entry):
+        _LOGGER.debug("OptionsFlowHandler init for config_entry %s", getattr(config_entry, "entry_id", config_entry))
+        # Use a private attribute because the base class exposes config_entry as a
+        # read-only property in some HA versions.
+        self._config_entry = config_entry
+
+    async def async_step_init(self, user_input: dict[str, Any] | None = None):
+        """Manage the options for the integration."""
+        errors: dict[str, str] = {}
+
+        current_interval = self._config_entry.options.get(
+            CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+        )
+
+        data_schema = vol.Schema(
+            {
+                vol.Optional(
+                    CONF_SCAN_INTERVAL, default=current_interval
+                ): vol.All(vol.Coerce(int), vol.Range(min=MIN_SCAN_INTERVAL, max=MAX_SCAN_INTERVAL))
+            }
+        )
+
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(step_id="init", data_schema=data_schema, errors=errors)
